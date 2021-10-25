52f64db226127dcfafddba7f154356a04d94b483
DevKing201 <hfmlarisa.pyr1989ld313jzc0@gmail.com>
2021-10-26 01:13:23 +0900

    script.js update

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -9639,6 +9639,29 @@ function initAlertEvent(element) {
 		}
   };
 
+  $(document).on('click', '.comment-reply', function(e) {
+    e.preventDefault();
+
+    var $this = $(this);
+    var url = $this.attr('href');
+    var saveurl = $this.data('save-url');
+    
+    $('#modal-comment-reply-form').attr('action', saveurl);
+    var $element = $('#ajax-comment-reply-form');
+    $element.load(url, function(response, status, xhr) {
+    })
+  });
+
+  $('#commentNewContent').text()
+  $('#commentNewContent').bind('input propertychange', function() {
+
+    $("#postbtn").attr('disabled', true);
+
+    if(this.value.length){
+      $("#postbtn").attr('disabled', false);
+    }
+});
+
   function getFocusableElements(menu) { // all visible focusable elements
     var elements = menu.element.querySelectorAll(focusableElString);
     menu.focusableElements = [];
