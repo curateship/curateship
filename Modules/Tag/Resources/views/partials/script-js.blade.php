e8830bcaaf9836cfaefd94984213ff840f083453
Serban Alexandru <serbann.alexandruu@yahoo.com>
2021-12-12 12:36:02 +0200

    Fix storage link

--- Modules/Tag/Resources/views/partials/script-js.blade.php
+++ Modules/Tag/Resources/views/partials/script-js.blade.php
@@ -173,7 +173,7 @@ var videojs_template =
           class: ImageTool,
           config: {
             endpoints: {
-              byFile: window.location.origin + '/editorjs/upload-image'
+              byFile: "{{ url('/editorjs/upload-image') }}"
             },
             additionalRequestHeaders : {
               'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -206,7 +206,7 @@ var videojs_template =
           class: ImageTool,
           config: {
             endpoints: {
-              byFile: window.location.origin + '/editorjs/upload-image'
+              byFile: "{{ url('/editorjs/upload-image') }}"
             },
             additionalRequestHeaders : {
               'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
