00fba4be6e5f7dc35a34bd4ceb4ba8005174d828
Digital Fox <11070693+digitalmamkin@users.noreply.github.com>
2021-12-20 03:03:27 +0400

    Masonry preloading fix;

--- resources/views/components/posts/lists/masonry-v3.blade.php
+++ resources/views/components/posts/lists/masonry-v3.blade.php
@@ -1,7 +1,7 @@
-<div class="container max-width-lg margin-top-md">
+<div class="container max-width-lg margin-top-md position-relative">
     <div class="preload-box" aria-hidden="true">
         <ul class="grid preload-grid gap-sm">
-            @for($i = 0 ; $i <= 8 ; $i++)
+            @for($i = 1 ; $i <= 12 ; $i++)
                 <li class="col-6 col-4@md">
                     <div class="ske ske--rect-16:9 margin-bottom-sm"></div>
                 </li>
@@ -10,29 +10,8 @@
     </div>
 
   <!--<ul class="masonry__list js-masonry__list js-infinite-scroll__content">-->
-  <div class="masonry-grid opacity-0">
-  @foreach($posts as $post)
-    <div class="grid-item">
-    @if($post->thumbnail)
-      <a class="thumb" href="{{ route('single-post-view', ['slug'   => $post->slug]) }}">
-        <figure class="card-v2">
-          <img class="block width-500% radius-md radius-bottom-right-0 radius-bottom-left-0" src="{{ $post->showThumbnail('medium') }}" alt="Image of {{ $post->title }}">
-          <figcaption class="card-v2__caption padding-x-sm padding-top-md padding-bottom-sm text-left">
+  <div class="masonry-grid">
 
-          </figcaption>
-        </figure>
-      </a>
-    @else
-      <span class="card__img card__img-cropped bg-opacity-50%"></span>
-      <div class="post-cell text-component line-height-xs v-space-xxs text-sm line-height-md">
-
-      </div>
-    @endif
-      <div class="user-cell">
-          <h3 class="text-xs padding-xs@md text-md@md"><a class="color-contrast-low" href="{{ route('single-post-view', ['slug' => $post->slug]) }}">{{ $post->title }}</a></h3>
-      </div>
-    </div>
-  @endforeach
   </div>
 
     <div class="loader-ellips">
