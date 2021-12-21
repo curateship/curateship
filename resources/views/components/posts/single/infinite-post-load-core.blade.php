e82b5f836daa29f8578acbb2729e3e7dd3e8f57f
Tyler <mobileglobalweb@gmail.com>
2021-12-21 13:51:43 -0500

    Clean up comment UI - front-end

--- resources/views/components/posts/single/infinite-post-load-core.blade.php
+++ resources/views/components/posts/single/infinite-post-load-core.blade.php
@@ -95,8 +95,8 @@
               @foreach($post->comments as $comment)
                 @if($comment->comment_status == 'published')
                   <li class="comments__comment">
-                    <div class="flex items-start margin-bottom-lg">
-                      <a href="#0" class="comments__author-img">
+                    <div class="flex items-start margin-bottom-lg author author--meta">
+                      <a href="#0" class="author__img-wrapper author author--meta">
                         @if($comment->avatar)
                           <img src="{{asset('storage/users-images/avatars/'.$comment->avatar)}}" alt="Author picture">
                         @else
@@ -112,15 +112,7 @@
 
                         <div class="margin-top-xs text-sm">
                           <div class="flex gap-xxs items-center">
-                            <button class="reset comments__vote-btn js-comments__vote-btn js-tab-focus" data-label="Like this comment along with 5 other people" aria-pressed="false">
-                              <span class="comments__vote-icon-wrapper">
-                                <svg class="icon block" viewBox="0 0 12 12" aria-hidden="true"><path d="M11.045,2.011a3.345,3.345,0,0,0-4.792,0c-.075.075-.15.225-.225.3-.075-.074-.15-.224-.225-.3a3.345,3.345,0,0,0-4.792,0,3.345,3.345,0,0,0,0,4.792l5.017,4.718L11.045,6.8A3.484,3.484,0,0,0,11.045,2.011Z"></path></svg>
-                              </span>
 
-                              <span class="margin-left-xxxs js-comments__vote-label" aria-hidden="true">5</span>
-                            </button>
-
-                            <span class="comments__inline-divider" aria-hidden="true"></span>
                             <a href="{{url('post/comment/reply/'.$comment->comment_id)}}" data-save-url="{{url('post/comment/reply-save/'.$comment->comment_id)}}" aria-controls="modal-comment-reply-{{$post->id}}" data-post-id="{{$post->id}}" role="button" class="comments__label-btn js-tab-focus comment-reply">Reply</a>
 
                             <span class="comments__inline-divider" aria-hidden="true"></span>
